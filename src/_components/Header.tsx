926d50d6a6774ae91f7707f6b719be3694b57122
adb-sh <git@adb.sh>
2024-01-06 18:36:21 +0100

    init

---
+++ src/_components/Header.tsx
@@ -0,0 +1,25 @@
+export default ({ ...props }) => (
+  <>
+    <header class="bg-white relative">
+      <nav class="container mx-auto flex justify-between items-center py-1">
+        <a class="flex items-center" href="/">
+          <img
+            src="/logo/cookbook-high-resolution-logo-black-transparent.svg"
+            alt="logo"
+            class="mx-3 h-16"
+          />
+        </a>
+      </nav>
+    </header>
+
+    <div class="sticky top-0 backdrop-blur bg-primary/80 dark:bg-primary-dark/75">
+      <div class="container overflow-auto mx-auto px-4">
+        <nav class="flex w-max">
+          <ul class="flex text-white font-normal">
+            <props.comp.HeaderMenuItems {...props} />
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </>
+);
